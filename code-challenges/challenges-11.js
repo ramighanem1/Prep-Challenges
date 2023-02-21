fa93d8b0de2fb916535c84b283044c651ddae198
ramighanem1 <rami.g.12@hotmail.com>
2023-02-21 16:58:10 +0300

    add my work

---
+++ code-challenges/challenges-11.js
@@ -0,0 +1,236 @@
+'use strict';
+
+// Important Note:
+// Kindly use map loop instead of for in all of your solutions
+
+// Resource:
+// Map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+// 
+// Given an array of values use map to calculate the square of the values.
+//  
+// Input: [ 2, 8, 3, 5 ]
+// Output: [ 4, 64, 9, 25 ]
+// 
+
+function square(arr) {
+    // write your code here
+    arr = arr.map((x) => {
+        return x * x;
+    });
+    return arr;
+
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Given an array of objects of students' first and last name, use the map to return an array off students' full names.
+//
+// note: the full names should be combined between student's first and last names.
+//
+// Input:
+// [
+//     {
+//         firstName: 'Adam',
+//         lastName: 'Anderson',
+//     },
+//     {
+//         firstName: 'Ben',
+//         lastName: 'Zeller',
+//     }, 
+//     {
+//         firstName: 'Peter',
+//         lastName: 'Mccord',
+//     },
+//     {
+//         firstName: 'Fred',
+//         lastName: 'Sagar',
+//     },
+//     {
+//         firstName: 'Nathan',
+//         lastName: 'Weiss',
+//     }
+// ]
+//
+// Output: ['Adam Anderson', 'Ben Zeller', 'Peter Mccord', 'Fred Sagar', 'Nathan Weiss']
+
+function fullName(arr) {
+    // write your code here
+    let FullName = arr.map((obj) => {
+        return obj.firstName + ' ' + obj.lastName;
+    });
+    return FullName;
+
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// optional:
+
+// Given an array of objects use map to calculate the average of the grades 
+// and return an array of the objects with a new property called avg (that represent the average of the grades).
+//  
+
+// Input:
+// var students = [
+// 	{
+// 			firstName: 'Adam',
+//      lastName: 'Anderson',
+// 			gradsList: [20, 50, 13, 11, 2, 45, 60, 29]
+// },
+// {
+// 			firstName: 'Ben',
+//      lastName: 'Zeller',
+// 			gradsList: [20, 10, 11, 11, 2, 5, 3, 2]
+// },
+// {
+// 			firstName: 'Peter',
+//      lastName: 'Mccord',
+// 	    gradsList: [62, 50, 80, 90, 39, 45, 60, 50]
+// },
+// {
+// 			firstName: 'Fred',
+//      lastName: 'Sagar',
+// 			gradsList: [20, 10, 18, 11, 2, 20, 3, 10]
+// }
+// ]
+//
+// Output:
+// [
+// 	{
+// 			firstName: 'Adam',
+//      lastName: 'Anderson',
+// 			gradsList: [20,50,13,11,2,45,60,29],
+// 			avg: 28.75
+// },
+// {
+// 			firstName: 'Ben',
+//      lastName: 'Zeller',
+// 			gradsList: [20,10,11,11,2,5,3,2],
+//			avg: 8
+// },
+// {
+// 			firstName: 'Peter',
+//      lastName: 'Mccord',
+// 	    gradsList: [62,50,80,90,39,45,60,50],
+//			avg: 59.5
+// },
+// {
+// 			firstName: 'Fred',
+//      lastName: 'Sagar',
+// 			gradsList: [20,10,18,11,2,20,3,10],
+//      avg: 11.75
+// 	}
+//]
+// -------------
+function gradesAvg(arr) {
+
+    let Result = arr.map((student) => {
+        let totalGrades = 0;
+        for (let i = 0; i < student.gradsList.length; i++) {
+            totalGrades += student.gradsList[i];
+        }
+        const avg = totalGrades / student.gradsList.length;
+        student.avg = avg;
+        return student;
+    });
+    return Result;
+}
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+//
+// Using the previous function result, determine if the student pass or failed,
+// and return the array of the objects with a new property called result (with Passed or Failed) 
+//
+// Note: if his/her avg equal or above 50 the result will be Passed, and Failed if it's less than that
+
+// Input:
+// var students = [
+// 	{
+// 			firstName: 'Adam',
+//          lastName: 'Anderson',
+// 			gradsList: [20,50,13,11,2,45,60,29],
+// 			avg: 28.75
+// },
+// {
+// 			firstName: 'Ben',
+//          lastName: 'Zeller',
+// 			gradsList: [20,10,11,11,2,5,3,2],
+//			avg: 8
+// },
+// {
+// 			firstName: 'Peter',
+//          lastName: 'Mccord',
+// 	        gradsList: [62,50,80,90,39,45,60,50],
+//			avg: 59.5
+// },
+// {
+// 			firstName: 'Fred',
+//          lastName: 'Sagar',
+// 			gradsList: [20,10,18,11,2,20,3,10],
+//          avg: 11.75
+// 	}
+//]
+// 
+// Output:
+// [
+// 	{
+// 			firstName: 'Adam',
+//      lastName: 'Anderson',
+// 			gradsList: [20,50,13,11,2,45,60,29],
+// 			avg: 28.75,
+//      result: 'Failed'
+// },
+// {
+// 			firstName: 'Ben',
+//      lastName: 'Zeller',
+// 			gradsList: [20,10,11,11,2,5,3,2],
+//			avg: 8,
+//      result: 'Failed'
+// },
+// {
+// 			firstName: 'Peter',
+//      lastName: 'Mccord',
+// 	    gradsList: [62,50,80,90,39,45,60,50],
+//			avg: 59.5,
+//      result: 'Passed'
+// },
+// {
+// 			firstName: 'Fred',
+//      lastName: 'Sagar',
+// 			gradsList: [20,10,18,11,2,20,3,10],
+//      avg: 11.75,
+//      result: 'Failed'
+// 	}
+// ]
+
+function studentsResult(arr) {
+    // write your code here
+    let objarr = gradesAvg(arr);
+    let Result = objarr.map((student) => {
+        if(student.avg >=50)
+        {
+            student.result = 'Passed';
+        }
+        else
+        {
+            student.result = 'Failed';
+        }
+        return student;
+    });
+    return Result;
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { square, fullName, gradesAvg, studentsResult };
